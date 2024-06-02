8c6994ae48d511d40813e095b93c0458eaff089b
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2024-06-02 12:26:45 -0500

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 # Replace these variables with your actual database credentials
-DB_USERNAME = 'your_actual_username'
-DB_PASSWORD = 'your_actual_password'
-DB_HOST = 'your_actual_host'
-DB_PORT = 'your_actual_port'  # e.g., '5432'
-DB_NAME = 'your_actual_db_name'
+DB_USERNAME = ''
+DB_PASSWORD = '
+DB_HOST = ''
+DB_PORT = ''  # e.g., '5432'
+DB_NAME = ''
 
 DATABASE_URL = f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
